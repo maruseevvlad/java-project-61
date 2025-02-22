c467b73293559a933952ad654c3ed6bc4cda12ae
maruseevvlad <maruseev.vlad@mail.ru>
2025-02-22 19:44:54 +0300

    rewrite random function in method isPrime

--- app/src/main/java/hexlet/code/games/IsPrimeGame.java
+++ app/src/main/java/hexlet/code/games/IsPrimeGame.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class IsPrimeGame extends Engine {
     public static void isPrime(String[] args) {
         Engine.greeting(args);
+        var  min = 10;
+        var  max = 100;
+        max -= min;
         while (userCount != NEEDWIN) {
             Scanner scanner = new Scanner(System.in);
-            var questionNumber = Math.round(1 - 0.5 + Math.random() * (100 - 2 + 1));
+            var questionNumber = (int) (Math.random() * ++max) + min;
             System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
             System.out.println("Question: " + questionNumber);
             var answer = scanner.nextLine();
