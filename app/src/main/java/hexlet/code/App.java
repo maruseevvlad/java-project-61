cc8fa2100ce655e403f864c3db7239dd779c56fd
maruseevvlad <maruseev.vlad@mail.ru>
2025-02-22 11:26:22 +0300

    added menu and greeting method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting(args);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n 1 - Greet\n 2 - Even\n 0 - Exit");
+        String gameNumber = scanner.next();
+
+            switch (gameNumber) {
+                case "0": System.out.println("Goodbye");
+                    scanner.close();
+                    break;
+                case "1": Greet.greeting(args);
+                    break;
+                default:
+                    System.out.println("'" + gameNumber + "' " + "is wrong answer ;(.");
+                    scanner.close();
+            }
+
+        
     }
 }
